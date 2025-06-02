b3ecd508991bac540b88acae0c8d0a8e5bc45304
Max Cerrina <mrcerrina@gmail.com>
2025-06-01 21:21:37 -0500

    voila

---
+++ script.js
@@ -0,0 +1,87 @@
+const toValidityMessage = (isValid) =>
+  `Status: ${isValid ? 'Valid' : 'Invalid'} JSON! ${isValid ? '✅' : '❌'}`;
+
+const getRandomEmoji = () =>
+  {
+    const randomEmoji = EMOJI_OPTIONS[Math.floor(Math.random() * EMOJI_OPTIONS.length)];
+
+    if (randomEmoji.charCodeAt(0) !== 55357) {
+      return getRandomEmoji();
+    }
+
+    return randomEmoji;
+  };
+
+const withPairedEmoji = (jsonyString) => {
+  let currentEmoji = null;
+  let stringWithReplacements = '';
+
+  for (let charIndex = 0; charIndex < jsonyString.length; charIndex += 1) {
+    const currentChar = jsonyString[charIndex];
+
+    const wasEscaped = jsonyString[charIndex - 1] === '\\';
+    const isEscapeChar = currentChar === '\\';
+    if (isEscapeChar) {
+      continue;
+    }
+
+    if (wasEscaped) {
+      stringWithReplacements += currentChar;
+      continue;
+    }
+
+    if (currentChar !== '"') {
+      stringWithReplacements += currentChar;
+      continue;
+    }
+
+    if (currentEmoji === null) {
+      currentEmoji = getRandomEmoji();
+      stringWithReplacements += currentEmoji;
+      continue;
+    }
+
+    stringWithReplacements += currentEmoji;
+    currentEmoji = null;
+  }
+
+  return stringWithReplacements;
+};
+
+function run() {
+  const jsonValidityElement = document.getElementById('json_validity_id');
+  if (!jsonValidityElement) {
+    throw new Error('missing json status element');
+  }
+
+  const outputElement = document.getElementById('json_output_id');
+  if (!outputElement) {
+    throw new Error('missing output element');
+  }
+
+  const inputElement = document.getElementById('json_input_id');
+  if (!inputElement) {
+    throw new Error('missing input element');
+  }
+  inputElement.value = '';
+
+  inputElement.addEventListener('input', (event) => {
+    const text = event.target.value;
+    jsonValidityElement.innerText = toValidityMessage(true);
+
+    try {
+      JSON.parse(text);
+
+      outputElement.innerText = withPairedEmoji(text);
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        jsonValidityElement.innerText = toValidityMessage(false);
+      } else {
+        console.error(error);
+        throw error;
+      }
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', run);
